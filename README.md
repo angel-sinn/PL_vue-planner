1e9dc3874149dadde5ea9776ee9bd849be1dfe97
Angel <angelkysinn@gmail.com>
2021-04-26 12:25:04 -0700

    Complete project

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
-# project-planner
+# Vue Practice 
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+!["Gif of Planner"](https://github.com/angel-sinn/PL_vue-planner/blob/master/docs/planner.gif)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <!-- $event is to get data that is being sent up -->
     <FilterNav @filterChange="current = $event" :current="current"/>
     <div v-if="projects.length">
-      <div v-for="project of projects" :key="project.id">
+      <div v-for="project of filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
       </div>
     </div>
@@ -30,17 +30,30 @@ export default {
       .catch(err => console.log(err.message))
   },
   methods: {
+    // make sure local data is up to date
     handleDelete(id) {
       this.projects = this.projects.filter((project) => {
         return project.id !== id
       })
     },
+    // make sure local data is up to date
     handleComplete(id) {
       let p = this.projects.find((project) => {
         return project.id === id
       })
       // update status
       p.complete = !p.complete
+    }
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter((project) => project.complete)
+      } 
+      if (this.current === 'ongoing') {
+        return this.projects.filter((project) => !project.complete)
+      }
+      return this.projects
     }
   }
 }
