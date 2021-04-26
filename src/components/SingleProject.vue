099776c468a69dededed22a573cea3cdff3e3011
Angel <angelkysinn@gmail.com>
2021-04-25 21:26:13 -0700

    Add single project component. Rendering each project and icon

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toggleDetails">{{project.title}}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{project.details}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  data() {
+    return {
+      showDetails: false,
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails
+    }
+  }
+}
+</script>
+
+<style>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+    border-left: 4px solid crimson;
+  }
+  h3 {
+    cursor: pointer;
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project of projects" :key="project.id">
+        <SingleProject :project="project"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'Home',
-  components: {}
+  components: {SingleProject},
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
